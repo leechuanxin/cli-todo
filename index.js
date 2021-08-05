80e8719cb696f41a2e9d72ef76562d67fcc921f1
Lee Chuan Xin <chuanxin@gametize.com>
2021-08-06 04:45:59 +0800

    feat: initial commit.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('hello world');
+import * as fileStorage from './jsonFileStorage.js';
+
+const INPUT = process.argv[2];
+const INIT_OBJ = { items: [] };
+const FILENAME = 'data.json';
+
+const handleReset = (err) => {
+  if (!err) {
+    console.log('Your to-do list has been successfully reset!');
+  }
+};
+
+if (INPUT === 'reset') {
+  fileStorage.write(FILENAME, INIT_OBJ, handleReset);
+} else {
+  console.log('Please enter a valid command: node index.js [reset|show|add "item"]');
+}
